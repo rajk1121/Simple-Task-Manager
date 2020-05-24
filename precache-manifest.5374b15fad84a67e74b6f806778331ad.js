7a5f947c92286a7570838a4ea74d7e8f4b956e08
rajk1121 <rajat.mishra1121@gmail.com>
2020-05-24 19:33:38 +0530

    Updates

---
+++ precache-manifest.5374b15fad84a67e74b6f806778331ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ccc86c8e92488465ba5fb759ff7691",
+    "url": "/Simple-Task-Manager/index.html"
+  },
+  {
+    "revision": "e14a60a9effbd9308df3",
+    "url": "/Simple-Task-Manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7e286e97d4859113087",
+    "url": "/Simple-Task-Manager/static/js/2.530555cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Task-Manager/static/js/2.530555cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14a60a9effbd9308df3",
+    "url": "/Simple-Task-Manager/static/js/main.f7ea23d4.chunk.js"
+  },
+  {
+    "revision": "6dea766ebb0da6621a9e",
+    "url": "/Simple-Task-Manager/static/js/runtime-main.612043db.js"
+  }
+]);
